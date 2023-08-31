7b8e95d9deebc4f392afffe287ed9ae2808caa35
fs-projects <fs.projectsuk@gmail.com>
2023-08-31 18:34:10 +0530

    Add: prettier lint-staged and husky precommit hook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 interface K {
-  name: string;
-  email: string;
+	name: string;
+	email: string;
 }
+
 console.log("hello word!");
+
 export const greet = "hello bhai!";
+
+interface J {
+	x: "Elon";
+	y: "Musk";
+}
